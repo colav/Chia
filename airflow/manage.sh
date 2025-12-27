dba6781f6453be71a1111fb10587aeb515d0f417
Omar Zapata <andresete.chaos@gmail.com>
2025-12-27 08:18:23 -0500

    deploy: add push option to manage.sh script

--- airflow/manage.sh
+++ airflow/manage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script para gestionar las instancias de Airflow (Dev/Prod)
-# Uso: ./manage.sh [dev|prod] [up|down|restart|logs|pull|ps]
+# Uso: ./manage.sh [dev|prod] [up|down|restart|logs|pull|ps|build|push]
 
 ENV=$1
 ACTION=${2:-up}
@@ -33,6 +33,20 @@ case $ACTION in
         ;;
     logs)
         docker compose -p "$PROJECT_NAME" --env-file "$ENV_FILE" logs -f --tail 100
+        ;;
+    build)
+        # Obtener el tag del archivo .env
+        TAG=$(grep AIRFLOW_IMAGE_TAG "$ENV_FILE" | cut -d'=' -f2)
+        TAG=${TAG:-latest}
+        echo "🛠 Construyendo imagen localmente con tag: colav/impactu_airflow:$TAG"
+        docker build -t "colav/impactu_airflow:$TAG" ../../impactu_airflow
+        ;;
+    push)
+        # Obtener el tag del archivo .env
+        TAG=$(grep AIRFLOW_IMAGE_TAG "$ENV_FILE" | cut -d'=' -f2)
+        TAG=${TAG:-latest}
+        echo "📤 Subiendo imagen a Docker Hub: colav/impactu_airflow:$TAG"
+        docker push "colav/impactu_airflow:$TAG"
         ;;
     *)
         docker compose -p "$PROJECT_NAME" --env-file "$ENV_FILE" $ACTION
